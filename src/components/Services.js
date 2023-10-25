e6794b5ac5268351439b498ae57caf16ddc1d182
M Girish Rao <121603439+Girish209@users.noreply.github.com>
2023-10-25 18:35:59 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -30,8 +30,8 @@ const Services = () => {
       <h2>Services</h2>
       <section>
         <motion.div className='servicebox1' whileInView={animations.whileInView} initial={animations.one}>
-            <h3>5+</h3>
-            <p>Years Experience</p>
+            <h3>Full-Stack</h3>
+            <p>Development</p>
         </motion.div>
         <motion.div className='servicebox2' whileInView={animations.whileInView} initial={animations.two}>
             <AiFillIeCircle/>
